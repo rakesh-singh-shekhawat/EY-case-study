7fb0dd77b9c2a91674418b31b92fbb22c3f8f01a
Singh <rakesh.j.singh@accenture.com>
2018-04-18 22:39:27 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RecordsComponent } from './records/records.component';
+import { UpdateRecordsComponent } from './update-records/update-records.component';
+import {RecordsService} from './records.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecordsComponent,
+    UpdateRecordsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:"", component: HomeComponent},
+      {path:"records", component: RecordsComponent},
+      {path:"UpdateRecords/:id", component: UpdateRecordsComponent}
+    ])
   ],
-  providers: [],
+  providers: [RecordsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
